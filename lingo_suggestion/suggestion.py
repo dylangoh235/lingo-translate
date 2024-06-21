f55be13118e2f2c162203e9337443ef5df21937f
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-06-21 18:54:29 +0900

    add int process

--- lingo_suggestion/suggestion.py
+++ lingo_suggestion/suggestion.py
@@ -49,8 +49,8 @@ class synonym_suggestion():
         else:
             return self.model.suggestion(target_word, text, cntxt_len)
 
-suggestor = synonym_suggestion("llama2_13b")
-term = suggestor.suggestion("0", True, 0, "what you wanna do my man? i don't need a baby right now, i just want to get a sleep.")
+# suggestor = synonym_suggestion("llama2_13b")
+# term = suggestor.suggestion("0", True, 0, "what you wanna do my man? i don't need a baby right now, i just want to get a sleep.")
 
-print(term)
+# print(term)
 
